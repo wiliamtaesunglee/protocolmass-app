bfb9d1af1eba020fed797ae0beaf2d4b55bb3c54
wiliamtaesunglee <wiliam.tae@gmail.com>
2019-08-15 09:36:07 -0300

    state counter added

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import HomeScreen from './src/screens/HomeScreen';
 import ComponentScreen from './src/screens/ComponentScreen';
 import ListScreen from './src/screens/ListScreen';
 import ImageScreen from './src/screens/ImageScreen';
-import CounterScreen from './src/screens/CounterScreen';
+import CounterScreen from './src/screens/CounterScreen'
 
 const navigator = createStackNavigator(
   {
--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {View, Text, StyleSheet, Button} from 'react-native'
 
 const CounterScreen = () => {
-    retrurn(
+
+    const [counter, setCounter] = useState(0)
+
+    return(
         <View>
+            <Button title='Increase'
+                onPress={() => setCounter(counter + 1)}
+            />
+            <Button title='Decrease'
+                onPress={() => setCounter(counter - 1)}
+            />
             <Text>
-                counter screen:
+                counter screen: {counter}
             </Text>
 
-            <Button 
-            title='press to incress + 1'
-            />
+            
         </View>
     )
 }
